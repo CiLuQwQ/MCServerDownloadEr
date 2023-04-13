76d42fa3d2729cf99a418445e6c10f28a085cd2e
7777a2333 <603484945@qq.com>
2023-04-13 21:14:41 +0800

    Sync

--- src/main/java/github/_7777a2333_/MCServerDownloadEr/Main/Main.java
+++ src/main/java/github/_7777a2333_/MCServerDownloadEr/Main/Main.java
@@ -10,7 +10,7 @@ public class Main {
 				.enableAllProtocol()
 				.buildSync();
 		//初始化蜗牛
-		String serverVersion, downloadLink = null,  legacyServer;
+		String serverVersion, downloadLink = null, legacyServer;
 		Scanner enter = new Scanner(System.in);
 		boolean cdn;
 		//初始化
@@ -20,21 +20,27 @@ public class Main {
 		System.out.println("赞助BMCLAPI:https://afdian.net/a/bangbang93");
 		System.out.println("其他见开源地址https://github.com/7777a2333/MCServerDownloadEr");
 		//显示说明
-		System.out.println("输入你要下载的服务端序号:)");
-		System.out.println("1   原版服务端   2 Purpur服务端(Paper分支)  3    Spigot服务端   4 CraftBukkit服务端");
-		System.out.println("5   MohistMC   6 Fabric服务端            7    远古版本服务端   8 PowerNukkitX基岩版服务端");
-		System.out.println("9   基岩版官方服务端(仅Windows 需要解压) 10 Forge安装器");
-		int type = enter.nextInt();
+		int type;
+		do {
+			System.out.println("输入你要下载的服务端序号:)");
+			System.out.println("1   原版服务端   2 Purpur服务端(Paper分支)  3    Spigot服务端   4 CraftBukkit服务端");
+			System.out.println("5   MohistMC   6 Fabric服务端            7    远古版本服务端   8 PowerNukkitX基岩版服务端");
+			System.out.println("9   基岩版官方服务端(仅Windows 需要解压) 10 Forge安装器");
+			type = enter.nextInt();
+		} while (type > 11);
 		//要求输入服务端类型
 		switch (type) {
-			case default -> System.out.println("输入错误!");
 			case 1 -> {
 				System.out.println("输入要下载的服务端版本号(正式版1.2.5及以上 快照13w16a以上)");
 				serverVersion = enter.next();
 				//要求输入服务端版本,并保存到服务端版本变量内
-				System.out.println("选择你要使用的镜像源:(填序号)");
-				System.out.println("1 Mojang源(海外) 2 BMCLAPI源(国内) 3 MCBBS源(国内)");
-				int a = enter.nextInt();
+				int a;
+				do {
+					System.out.println("选择你要使用的镜像源:(填序号)");
+					System.out.println("1 Mojang源(海外) 2 BMCLAPI源(国内) 3 MCBBS源(国内)");
+					a = enter.nextInt();
+				} while (a > 4);
+
 				//输入下载源编号
 				switch (a) {
 					case 1 -> downloadLink = "https://www.mcjars.com/get/vanilla-" + serverVersion + ".jar";
@@ -110,19 +116,25 @@ public class Main {
 				System.out.println("将下载PNX-CLI-0.0.8");
 				downloadLink = "https://github.com/PowerNukkitX/PNX-CLI/releases/download/0.0.8/PNX-CLI-0.0.8.jar";
 			}
-			case 9 ->{
-				System.out.println("请输入服务端版本(如1.19.63.01)");
+			case 9 -> {
+				System.out.println("请输入服务端版本(如1.19.63.01,1.20.0.20)");
 				serverVersion = enter.next();
-				downloadLink = "https://minecraft.azureedge.net/bin-win/bedrock-server-" + serverVersion + ".zip";
+				System.out.println("是不是Preview版？(是输入true/否输入false)");
+				boolean isPreview = enter.nextBoolean();
+				if (isPreview) {
+					downloadLink = "https://minecraft.azureedge.net/bin-win/bedrock-server-" + serverVersion + ".zip";
+				} else {
+					downloadLink = "https://minecraft.azureedge.net/bin-win-preview/bedrock-server-" + serverVersion + ".zip";
+				}
 			}
-			case 10->{
+			case 10 -> {
 				System.out.println("输入要下载的版本号(1.1及以上)");
 				serverVersion = enter.next();
 				//要求输入服务端版本,并保存到服务端版本变量内
 
 				System.out.println("输入要下载的Forge版本(可使用启动器查看)");
 				String forgeVersion = enter.next();
-				downloadLink = "https://maven.minecraftforge.net/net/minecraftforge/forge/" + serverVersion +"-" + forgeVersion +"/forge-"  + serverVersion + "-" + forgeVersion +"-installer.jar";
+				downloadLink = "https://maven.minecraftforge.net/net/minecraftforge/forge/" + serverVersion + "-" + forgeVersion + "/forge-" + serverVersion + "-" + forgeVersion + "-installer.jar";
 
 			}
 
